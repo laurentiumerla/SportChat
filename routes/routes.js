376506349de82e26aa8263dfc51790aee5e221f9
Merla <I334691@global.corp.sap>
2016-11-29 15:04:24 +0200

    Initial Commit

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+// var ChatModel = require("../models/chatmodel");
+
+// var appRouter = function(app) {
+
+//     app.get("/fetch", function(req, res) {
+//         ChatModel.getAll(function(error, result) {
+//             if(error) {
+//                 return res.status(400).send(error);
+//             }
+//             return res.send(result);
+//         });
+//     });
+
+// };
--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 
-// Creat cu mandrie in Romania - Laurentiu Merla
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
+////////////////////////////////////////////////////////////
+// Creat cu mandrie in Romania - Laurentiu Cristian Merla //
+////////////////////////////////////////////////////////////
 
-app.listen(3000);
+var express = require("express");
+var bodyParser = require("body-parser");
+// var couchbase = require("couchbase");
+var path = require("path");
+// var config = require("./config");
+var app = express();
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+var server = require("http").Server(app);
+var io = require("socket.io").listen(server);
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+// module.exports.bucket = (new couchbase.Cluster(config.couchbase.server)).openBucket(config.couchbase.bucket);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/scripts", express.static(__dirname + "/node_modules/"));
+
+// var routes = require("./routes/routes.js")(app);
+// var ChatModel = require("./models/chatmodel.js");
+
+io.on("connection", function (socket) {
+    socket.on("chat_message", function (msg) {
+        console.log(JSON.stringify(msg));
+        // ChatModel.create({message: msg}, function(error, result) {
+        //     if(error) {
+        //         console.log(JSON.stringify(error));
+        //     }
+        //     io.emit("chat_message", msg);
+        // });
+    });
+});
+
+server.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
 });
